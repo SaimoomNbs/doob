9fe626acefcdff1c02bb758856546e7f24a860a5
SaimoomNbs <newboysaim@gmail.com>
2023-06-11 15:54:46 +0600

    move to main

--- doob-profile.php
+++ doob-profile.php
@@ -1,19 +1,12 @@
 <!doctype html>
 <html lang="en">
-
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="theme-color" content="#000000">
     <title>Doob</title>
-    <meta name="description" content="Finapp HTML Mobile Template">
-    <meta name="keywords" content="bootstrap, wallet, banking, fintech mobile template, cordova, phonegap, mobile, html, responsive" />
     <link rel="icon" type="image/png" href="assets/img/logo.svg.svg" sizes="32x32">
-    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/icon/192x192.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/logo.svg.svg">
     <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="manifest" href="__manifest.json">
 </head>
 
 <body>
@@ -32,16 +25,7 @@
     <!-- App Capsule -->
     <div id="appCapsule">
 
-        <div class="section mt-3 text-center">
-            <div class="avatar-section">
-                <a href="#">
-                    <img src="assets/img/sample/avatar/avatar1.jpg" alt="avatar" class="imaged w100 rounded">
-                    <span class="button">
-                        <ion-icon name="camera-outline"></ion-icon>
-                    </span>
-                </a>
-            </div>
-        </div>
+
 
     </div>
     <!-- * App Capsule -->
@@ -50,7 +34,7 @@
     <!-- App Bottom Menu -->
     <?php include 'menu.php'; ?>
     <!-- * App Bottom Menu -->
-
+    
     <!-- App Sidebar -->
     <?php include 'sidebar.php'; ?>
     <!-- * App Sidebar -->
@@ -66,11 +50,6 @@
     <script src="assets/js/plugins/splide/splide.min.js"></script>
     <!-- Base Js File -->
     <script src="assets/js/base.js"></script>
-
-    <!-- <script>
-        // Add to Home with 2 seconds delay.
-        AddtoHome("2000", "once");
-    </script> -->
 
 </body>
 
